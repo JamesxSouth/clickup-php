8a77e9e287f67961cee956c5bb16329bc652d65a
James South <james.south@nudgedigital.co.uk>
2020-05-06 12:45:36 +0100

    Update API version

--- src/ClickUp/Client.php
+++ src/ClickUp/Client.php
@@ -2,101 +2,102 @@
 
 namespace ClickUp;
 
+use GuzzleHttp\Client;
 use ClickUp\Objects\TaskFinder;
 use ClickUp\Objects\Team;
 use ClickUp\Objects\TeamCollection;
 use ClickUp\Objects\User;
 
-class Client
-{
-	private $guzzleClient;
+/**
+ *
+ */
+class Client {
+  private $guzzleClient;
 
-	public function __construct($apiToken)
-	{
-		$this->guzzleClient = new \GuzzleHttp\Client([
-			'base_uri' => 'https://api.clickup.com/api/v1/',
-			'headers' => [
-				'Authorization' => $apiToken,
-			]
-		]);
-	}
+  /**
+   *
+   */
+  public function __construct($apiToken) {
+    $this->guzzleClient = new Client([
+      'base_uri' => 'https://api.clickup.com/api/v2/',
+      'headers' => [
+        'Authorization' => $apiToken,
+      ],
+    ]);
+  }
 
-	public function client()
-	{
-		return $this;
-	}
+  /**
+   *
+   */
+  public function client() {
+    return $this;
+  }
 
-	/**
-	 * @return User
-	 */
-	public function user()
-	{
-		return new User(
-			$this,
-			$this->get('user')['user']
-		);
-	}
+  /**
+   * @return \ClickUp\Objects\User
+   */
+  public function user() {
+    return new User(
+    $this,
+    $this->get('user')['user']
+    );
+  }
 
-	/**
-	 * @return TeamCollection
-	 */
-	public function teams()
-	{
-		return new TeamCollection(
-			$this,
-			$this->get('team')['teams']
-		);
-	}
+  /**
+   * @return \ClickUp\Objects\TeamCollection
+   */
+  public function teams() {
+    return new TeamCollection(
+    $this,
+    $this->get('team')['teams']
+    );
+  }
 
-	/**
-	 * @param int $teamId
-	 * @return Team
-	 */
-	public function team($teamId)
-	{
-		return new Team(
-			$this,
-			$this->get("team/$teamId")['team']
-		);
-	}
+  /**
+   * @param int $teamId
+   * @return \ClickUp\Objects\Team
+   */
+  public function team($teamId) {
+    return new Team(
+    $this,
+    $this->get("team/$teamId")['team']
+    );
+  }
 
-	/**
-	 * @param int $teamId
-	 * @return TaskFinder
-	 */
-	public function taskFinder($teamId)
-	{
-		return new TaskFinder($this, $teamId);
-	}
+  /**
+   * @param int $teamId
+   * @return \ClickUp\Objects\TaskFinder
+   */
+  public function taskFinder($teamId) {
+    return new TaskFinder($this, $teamId);
+  }
 
-	/**
-	 * @param string $method
-	 * @param array  $params
-	 * @return mixed
-	 */
-	public function get($method, $params = [])
-	{
-		$response = $this->guzzleClient->request('GET', $method, ['query' => $params]);
-		return \GuzzleHttp\json_decode($response->getBody(), true);
-	}
+  /**
+   * @param string $method
+   * @param array $params
+   * @return mixed
+   */
+  public function get($method, $params = []) {
+    $response = $this->guzzleClient->request('GET', $method, ['query' => $params]);
+    return \GuzzleHttp\json_decode($response->getBody(), TRUE);
+  }
 
-	/**
-	 * @param string $method
-	 * @param array $body
-	 * @return mixed
-	 */
-	public function post($method, $body = [])
-	{
-		return \GuzzleHttp\json_decode($this->guzzleClient->request('POST', $method, ['json' => $body])->getBody(), true);
-	}
+  /**
+   * @param string $method
+   * @param array $body
+   * @return mixed
+   */
+  public function post($method, $body = []) {
+    return \GuzzleHttp\json_decode($this->guzzleClient->request('POST', $method, ['json' => $body])->getBody(), TRUE);
+  }
 
-	/**
-	 * @param string $method
-	 * @param array $body
-	 * @return mixed
-	 */
-	public function put($method, $body = [])
-	{
-		return \GuzzleHttp\json_decode($this->guzzleClient->request('PUT', $method, ['json' => $body])->getBody(), true);
-	}
+  /**
+   * @param string $method
+   * @param array $body
+   * @return mixed
+   */
+  public function put($method, $body = []) {
+    return \GuzzleHttp\json_decode($this->guzzleClient->request('PUT', $method, ['json' => $body])->getBody(), TRUE);
+  }
+
 }
